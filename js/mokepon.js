d102489971a6439493a9e903a2997bce6689ecc1
Genny Sandoval <gennylisandoval@gmail.com>
2023-02-01 11:35:00 -0500

    creé los arreglos con los ataques´dándole prioridad según el elemento de cada mokepon

--- js/mokepon.js
+++ js/mokepon.js
@@ -28,15 +28,37 @@ class Mokepon {
         this.nombre = nombre
         this.foto = foto
         this.vida = vida 
+        this.ataques = []
     }
 }
 
-let hipodoge = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 3)
-let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 3)
-let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 3)
+let hipodoge = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 5)
+let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5)
+let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5)
 
-mokepones.push('hipodoge', 'capipepo', 'ratigueya')
-console.log(mokepones)
+hipodoge.ataques.push(
+    { nombre: '💦', id: 'boton-agua' },
+    { nombre: '💦', id: 'boton-agua' },
+    { nombre: '💦', id: 'boton-agua' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '🌎', id: 'boton-tierra' },
+)
+
+capipepo.ataques.push(
+    { nombre: '🌎', id: 'boton-tierra' },
+    { nombre: '🌎', id: 'boton-tierra' },
+    { nombre: '🌎', id: 'boton-tierra' },
+    { nombre: '💦', id: 'boton-agua' },
+    { nombre: '🔥', id: 'boton-fuego' },
+)
+
+ratigueya.ataques.push(
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '💦', id: 'boton-agua' },
+    { nombre: '🌎', id: 'boton-tierra' },
+)
 
 function iniciarJuego() {
     sectionSeleccionarAtaque.style.display = "none"
